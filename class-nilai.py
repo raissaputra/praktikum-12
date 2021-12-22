e9b4f2cbdc423a7a2d9c0bf68e4c7cbbdd6d2e53
raissaputra <raissaputra02@gmail.com>
2021-12-22 20:05:29 +0700

    update program

--- class-nilai.py
+++ class-nilai.py
@@ -39,21 +39,26 @@ class Nilai:
     def hapus(self):
         pass
 
+    def ubah(self):
+        pass
+
 
 # INSTANCE CLASS
 mhs1 = Nilai("12345", "Alfin Cou", "Teknik Informatika")
 print("| --Tampilkan Data-- |")
 mhs1.tampilkan()
+
 # UBAH NILAI ATRIBUT
 mhs1.set_Tugas(100)
-mhs1.set_uts(80)
-mhs1.set_uas(80)
-print("\n| --Cetak Data Nilai-- |")
+mhs1.set_uts(50)
+mhs1.set_uas(90)
+
+print("\n| --Cetak Data Nilai yang sudah di ubah-- |")
+mhs1.tampilkan()
 print(("Nilai tugas :"), mhs1.get_Tugas())
 print(("Nilai uts :"), mhs1.get_uts())
 print(("Nilai uas :"), mhs1.get_uas())
+
 # TAMPILKAN NILAI AKHIR
 print("\n| --Tampilkan Perhitungan Nilai Akhir-- |")
 print(("Nilai Akhir :"), mhs1.cetak())
-
-mhs1.nama
